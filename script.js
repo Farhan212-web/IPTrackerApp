f508156bd773a55d3c4bc6436484b7bf10aa9e45
Farhan Emad <farhan48034@gmail.com>
2022-03-12 01:11:36 +0000

    updated node dependancies making eslint work

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ L.marker([0, 0]).addTo(map)
 function checkIfValidIP(str) {
     // Regular expression to check if string is a IP address
     const regexExp = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
-    
     return regexExp.test(str);
 }
 
